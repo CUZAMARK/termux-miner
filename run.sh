e119738f2c69a3b17421173194b4571ea16878ec
CUZAMARK <110730813+CUZAMARK@users.noreply.github.com>
2025-02-24 06:16:45 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -e
 # Replace changeme with your values. Check out readme for more.
-POOL="changeme"
-PORT=changeme
-MYADDRESS="changeme"
-COINNAME="changeme"
+POOL="cfx-asia1.nanopool.org"
+PORT=10543
+MYADDRESS="WrkzpAXkeEzQu6n6BYZuxrjf2chEaHBRbhnQNL667bwMWWDsXD1xNnHTck2NBBMmcpVbuNm5ppc1kj3hSmWJ566M8eK5wZRaYv"
+COINNAME="conflux"
 
 cd xmrig/build
 
